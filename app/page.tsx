a47349032e3b5fc0f4a06ef0e5b420b60391eaba
ritika-b <rbharati3@gatech.edu>
2024-02-11 22:05:30 -0500

    content

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
       <h2 className={`text-2xl font-semibold`}>Team Members</h2>
       <p className={`text-sm opacity-50`}>
         <ul>
-          <li>Zainab Akhtar: Took responsibility of understanding the ACLED CAST forecasting system by conducting in-depth research via website, research paper, YouTube videos and manually downloading data. I was able to identify the different aspects i.e spatial/temporal needed and brainstormed some ideas to move forward with metric creation. </li>
-          <li>Raj Srivastava: Took the Conflict Forecast forecasting system, conducted in-depth research, identified the pros/cons of the dataset in terms of time and geographical context. </li>
-          <li>Austin Walker: Evaluated the ViEWS (Violence & Impacts Early Warning System) forecasting system, including an in depth look at several white papers and presentations explaining what criteria the ViEWS forecast considers, how the ViEWS forecast manipulates data, and what data is returned by the ViEWS forecast. Also, evaluated sample outputs from the ViEWS forecast to see how data is organize and identified the ViEWS API which can/will be used later in the project. </li>
-          <li>Ritika Bharati: Utilized Vercel and Github, to setup the team webpage which can be found here: https://undp-fpv.vercel.app/</li>
+          <li>Zainab Akhtar: Research and development. More information coming soon! </li>
+          <li>Raj Srivastava: Research and development. More information coming soon! </li>
+          <li>Austin Walker: Research and development. More information coming soon! </li>
+          <li>Ritika Bharati: Research and development. More information coming soon!</li>
         </ul>
       </p>
 
