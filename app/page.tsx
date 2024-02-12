5a35a60f1529a1a006da0ddb21db214413196f43
ritika-b <rbharati3@gatech.edu>
2024-02-11 21:59:09 -0500

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
       <h1>UNDP Forecasting Political Violence Page</h1>
 
       <h2 className={`mb-3 text-2xl font-semibold`}> Partner Objectives </h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}> An integrated approach that seamlessly integrates heterogeneous forecasting outputs to offer a novel resource for uncovering new insights and patterns in political violence, so researchers, policymakers, and the international community can address and mitigate the impacts of political violence effectively. </p>
+      <p className={`m-0 text-sm opacity-50`}> An integrated approach that seamlessly integrates heterogeneous forecasting outputs to offer a novel resource for uncovering new insights and patterns in political violence, so researchers, policymakers, and the international community can address and mitigate the impacts of political violence effectively. </p>
 
       <h2 className={`mb-3 text-2xl font-semibold`}>Goals</h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>The project aims to tackle the challenge of consolidating diverse conflict forecasting outputs from three well-established sources: ACLED, VIEWS, and Conflict Forecast. The primary objective is to create an analytical engine that utilizes advanced integration metrics and employs various techniques, such as statistical and machine learning methods. The goal is to synthesize the data from these sources into a unified and simplified model, yielding a more accurate and simpler to understand forecast of political violence. </p>
+      <p className={`m-0 text-sm opacity-50`}>The project aims to tackle the challenge of consolidating diverse conflict forecasting outputs from three well-established sources: ACLED, VIEWS, and Conflict Forecast. The primary objective is to create an analytical engine that utilizes advanced integration metrics and employs various techniques, such as statistical and machine learning methods. The goal is to synthesize the data from these sources into a unified and simplified model, yielding a more accurate and simpler to understand forecast of political violence. </p>
 
       <h2 className={`mb-3 text-2xl font-semibold`}>Team Members</h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      <p className={`m-0 text-sm opacity-50`}>
         <ul>
           <li>Zainab Akhtar: Took responsibility of understanding the ACLED CAST forecasting system by conducting in-depth research via website, research paper, YouTube videos and manually downloading data. I was able to identify the different aspects i.e spatial/temporal needed and brainstormed some ideas to move forward with metric creation. </li>
           <li>Raj Srivastava: Took the Conflict Forecast forecasting system, conducted in-depth research, identified the pros/cons of the dataset in terms of time and geographical context. </li>
@@ -22,7 +22,7 @@ export default function Home() {
       </p>
 
       <h2 className={`mb-3 text-2xl font-semibold`}>Lighthouse Metrics</h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      <p className={`m-0 text-sm opacity-50`}>
         <ul>
           <li>Performance: 100 </li>
           <li>Accessibility: 100 </li>
