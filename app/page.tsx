4aa21d7ef01f2b3029a07e2443b6a15d6f4b2a64
ritika-b <rbharati3@gatech.edu>
2024-03-29 22:34:28 -0400

    SEO + Updates

--- app/page.tsx
+++ app/page.tsx
@@ -13,14 +13,14 @@ export default function Home() {
     <ScrollUp />
     <main>
       <meta name="theme-color" content="#317EFB"/>
-      <h1 className={`text-2xl font-semibold`}> Forecasting Political Violence</h1>
+      <h1 className={`text-2xl font-semibold`}> OMSCS C4G Project: Forecasting Political Violence</h1>
       <br></br>
       <p className={`text-sm opacity-75`}> Test our project out <a href="https://fpv-dashboard-cwaa.onrender.com/"> here</a>.
       </p>
       <p className={`text-sm opacity-75`}>Give us feedback <a href="https://forms.gle/QxXdmpbExYP8qeqQA">here</a>.</p>
       <br></br>
       <h2 className={`text-1xl font-semibold`}> Partner Objectives </h2>
-      <p className={`text-sm opacity-75`}> An integrated approach that seamlessly integrates heterogeneous forecasting outputs to offer a novel resource for uncovering new insights and patterns in political violence, so researchers, policymakers, and the international community can address and mitigate the impacts of political violence effectively. </p>
+      <p className={`text-sm opacity-75`}> An integrated approach that seamlessly integrates heterogeneous forecasting outputs to offer a novel resource for uncovering new insights and patterns in political violence, so researchers, policymakers, and the international community can address and mitigate the impacts of political violence effectively on a global scale. </p>
       <br></br>
       <h2 className={`text-1xl font-semibold`}>Goals</h2>
       <p className={`text-sm opacity-75`}>The project aims to tackle the challenge of consolidating diverse conflict forecasting outputs from three well-established sources: ACLED, VIEWS, and Conflict Forecast. The primary objective is to create an analytical engine that utilizes advanced integration metrics and employs various techniques, such as statistical and machine learning methods. The goal is to synthesize the data from these sources into a unified and simplified model, yielding a more accurate and simpler to understand forecast of political violence. </p>
@@ -54,10 +54,21 @@ export default function Home() {
         </ul>
       </p>
       <br></br>
-      <h2 className={`text-1xl font-semibold`}>Weekly Updates</h2>
+      <h2 className={`text-1xl font-semibold`}>Weekly Project Updates</h2>
+
+      <h3 className={`ßfont-semibold`}>Week of 1st April 2024</h3>
+      <p className={`text-sm opacity-75`}>
+        The field evaluation instructions will be prepared, the team will meet the stakeholders for an evaluation of the dashboard where a set of tasks will be given to them. Their evaluation will be recorded as well as the feedback they give needed for P7-FieldEvaluation. The feedback will be incorporated to further improve the MVP.
+      </p>
+      <br></br>
+      <h3 className={`ßfont-semibold`}>Week of 24th March 2024</h3>
+      <p className={`text-sm opacity-75`}>
+        The team focused on resolving the main loading error of our MVP, and incorporating feedback we received from our peer evaluations. The team met with UNDP and the TA to receive further feedback on our MVP in order to prepare for the demo assignment.
+      </p>
+      <br></br>
       <h3 className={`ßfont-semibold`}>Week of 11th March 2024</h3>
       <p className={`text-sm opacity-75`}>
-        There are two main tasks which will be worked upon in parallel: 1) Conducting an evaluation by selecting specific events i.e. Gaza/u=Ukraine to test the hypothesis: does having an ensemble forecast make sense as compared to individual forecasts? Then a list of all possible ensemble forecast methodologies will be created and discussed. 2) Dashboard Development in Plotly Dash where the initial tasks include the setup up a basic authentication, deployment on Render.com, and a basic web app implementation with the current data we have.
+        There are two main tasks which will be worked upon in parallel: 1) Conducting an evaluation by selecting specific political violence events i.e. Gaza/u=Ukraine to test the hypothesis: does having an ensemble forecast make sense as compared to individual forecasts? Then a list of all possible ensemble forecast methodologies will be created and discussed. 2) Dashboard Development in Plotly Dash where the initial tasks include the setup up a basic authentication, deployment on Render.com, and a basic web app implementation with the current data we have.
       </p>
       <br></br>
       <h3 className={`ßfont-semibold`}>Week of 4th March 2024</h3>
